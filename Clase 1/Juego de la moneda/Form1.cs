f84ebba2947f043ecc6350a0b30cc00be3d181d5
Franco Balich <franco.balich@gmail.com>
2023-04-09 14:09:16 -0300

    Add 'Juego de la moneda' project

---
+++ Clase 1/Juego de la moneda/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_de_la_moneda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            Random genRandom = new Random((int)DateTime.Now.Ticks);
+            b numRandom = genRandom.Next(0, 1 + 1);
+
+            if (numRandom == 1){
+                pbxCara.Show();
+                pbxCruz.Hide();
+            }
+            else{
+                pbxCara.Hide();
+                pbxCruz.Show();
+            }
+        }
+    }
+}
