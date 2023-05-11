0b4b341cf11d60e8f2e8025285db614e27d8067e
Franco Balich <franco.balich@gmail.com>
2023-05-11 17:55:58 -0300

    Add classes

--- Clase 3/Simulador de personaje/Form1.cs
+++ Clase 3/Simulador de personaje/Form1.cs
@@ -12,67 +12,35 @@ namespace Simulador_de_personaje
 {
     public partial class Form1 : Form
     {
+        private Personaje robot = new Personaje("Roby");
+
         public Form1()
         {
             InitializeComponent();
         }
-        private int generarNumeroRandom(int min, int max)
-        {
-            Random genRandom = new Random((int)DateTime.Now.Ticks);
-            int numero = genRandom.Next(min, max + 1);
-            return numero;
-        }
-        private void atacar()
-        {
-            int daño= generarNumeroRandom(100, 1000);
-            MessageBox.Show("El personaje ataco con " + daño.ToString() +" de daño." , "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        private void defender()
-        {
-            int resultado = generarNumeroRandom(0, 1);
-            if (resultado == 1){
-                MessageBox.Show("El personaje se defendio correctamente", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else{
-                MessageBox.Show("El personaje no se pudo defender.", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-           
-        }
-        private void dormir()
-        {
-            MessageBox.Show("El personaje dormio", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        private void soltarItem()
-        {
-            MessageBox.Show("El personaje solto un item", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        private void tomarItem()
-        {
-            MessageBox.Show("El personaje tomo un item", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
         private void btnAtacar_Click(object sender, EventArgs e)
         {
-            atacar();
+            robot.atacar();
         }
 
         private void btnDefender_Click(object sender, EventArgs e)
         {
-            defender();
+            robot.defender();
         }
 
         private void btnDormir_Click(object sender, EventArgs e)
         {
-            dormir();
+            robot.dormir();
         }
 
         private void btnTomar_Click(object sender, EventArgs e)
         {
-            tomarItem();
+            robot.tomarItem("escudo");
         }
 
         private void btnSoltar_Click(object sender, EventArgs e)
         {
-            soltarItem();
+            robot.soltarItem("tornillo");
         }
     }
 }
---
+++ Clase 3/Simulador de personaje/Personaje.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulador_de_personaje
+{
+    public class Personaje
+    {
+        private string nombre="";
+
+        public Personaje(string pNombre) 
+        {
+            this.nombre = pNombre;
+        }
+
+        private int generarNumeroRandom(int min, int max)
+        {
+            Random genRandom = new Random((int)DateTime.Now.Ticks);
+            int numero = genRandom.Next(min, max + 1);
+            return numero;
+        }
+
+        public void atacar()
+        {
+            int daño = generarNumeroRandom(100, 1000);
+            MessageBox.Show($"{this.nombre} ataco con " + daño.ToString() + " de daño.", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void defender()
+        {
+            int resultado = generarNumeroRandom(0, 1);
+            if (resultado == 1)
+            {
+                MessageBox.Show($"{this.nombre} se defendio correctamente", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"{this.nombre} no se pudo defender.", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+        public void dormir()
+        {
+            MessageBox.Show($"{this.nombre} se durmio", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void soltarItem(string pItem)
+        {
+            MessageBox.Show($"{this.nombre} solto un {pItem}", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void tomarItem(string pItem)
+        {
+            MessageBox.Show($"{this.nombre} tomo un {pItem}", "Accion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
